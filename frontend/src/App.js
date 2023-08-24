995ac93424c2a3f4138bee255299c4b9a7ca0ea0
meniac <97505197+AriLaurin@users.noreply.github.com>
2023-08-24 15:01:38 +0200

    imported initial react app

---
+++ frontend/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+  return (
+<div class="container">
+  <div class="main-cont"></div>
+  <div class="center">
+    <h1 className="home-title">Welcome</h1>
+  </div>
+  <div class="left-bar"></div>
+  <div class="right-bar"></div>
+  <div class="nav-bar"></div>
+  <div class="footer"></div>
+</div>
+  );
+}
+
+export default App;
